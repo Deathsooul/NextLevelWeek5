88baa16f42ad68d81af348639228ecb015dd7371
Wallace <wbescrovaine@gmail.com>
2021-06-19 02:34:33 -0300

    Criando entidades

---
+++ TrilhaNode/src/entities/Setting.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryColumn,
+} from "typeorm";
+
+// a versao 4 usa numeros aleatorios para gerar o uuid
+import { v4 as uuid } from "uuid";
+
+@Entity("settings")
+class Setting {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  chat: boolean;
+
+  @CreateDateColumn()
+  updated_at: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    // Se nao tiver preenchido, preenchemos com um uuid
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Setting };
