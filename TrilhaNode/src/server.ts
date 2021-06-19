8c32d43a838584de67442add6aec24bf884b57dd
Wallace <wbescrovaine@gmail.com>
2021-06-19 01:57:05 -0300

    Adicionando alguns ajustes

--- TrilhaNode/src/server.ts
+++ TrilhaNode/src/server.ts
@@ -4,17 +4,25 @@ import "./database";
 
 const app = express();
 
-app.get("/", (request, response)=>{
-    return response.json({
-        message:"Salve putada"
-    })
-})
+/**
+ * Métodos padrão
+ * GET -> Buscas
+ * POST -> Criaçào
+ * PUT ->Alteração
+ * DELETE -> Deletar
+ * PATCH -> Alterar uma informaçào especifica
+ */
 
-app.post("/", (resquest, repsonse)=>{
-    return response.json({
-        message:"Usuário Salvo"
-    })
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Salve putada",
+  });
+});
 
-})
+app.post("/", (resquest, repsonse) => {
+  return response.json({
+    message: "Usuário Salvo",
+  });
+});
 
-app.listen(3333, ()=> console.log("Server is running on port 3333"))
+app.listen(3333, () => console.log("Server is running on port 3333"));
