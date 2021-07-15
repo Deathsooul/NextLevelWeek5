0c72e09d975d6d501120a75932ccbf3626bac1b0
Wallace <wbescrovaine@gmail.com>
2021-07-14 23:55:13 -0300

    Message Part

--- TrilhaNode/src/routes.ts
+++ TrilhaNode/src/routes.ts
@@ -1,5 +1,6 @@
 // Imports
 import { Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
 import { UsersController } from "./controllers/UsersController";
 
@@ -8,10 +9,15 @@ const routes = Router();
 // Import controllers
 const settingsController = new SettingsController();
 const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 // Rotas
 
 routes.post("/settings", settingsController.create);
+
 routes.post("/users", usersController.create);
+
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showByUser);
 
 export { routes };
